e580716194c3477d0cfef2e2fec05f917f108630
Juan Valença de Oliveira <starbea2405@gmail.com>
2024-10-09 14:38:47 -0300

    Add files via upload

---
+++ School_test.py
@@ -0,0 +1,6 @@
+#Pergunta ao usuário qual a sua escola 
+Escola = input ("qual a sua escola?")
+#pergunta ao usuário qual é o seu nome
+Nome = input("qual o seu nome?")
+#texto completo
+print(f"Olá, {Nome}, do(a) {Escola}. Bem-vindo(a) ao nosso sistema")
