9a4b7ca39b19c24b0a31dda963354c4636f92c09
Juan Valença de Oliveira <starbea2405@gmail.com>
2024-10-23 14:07:09 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,18 @@
+#solicitar valores  a serem calculador
+x = int(input("informe o valor:"))
+y = int(input("informe o valor:"))
+
+#Realizar o Cálculo
+resultado = int(x) + int(y)
+
+#Exibir o resultado para o usuário
+
+print(f" {x} + {y} = {resultado}")
+
+#exemplo do que fazer
+#qual
+#if soma:
+#if menos:
+#if dividir
+#
+#
---
+++ Nova_MateMáquina.py
@@ -0,0 +1,102 @@
+#Só dizendo oq são as coisas: "if, elif e else" são estruturas de controle, Operadores matemáticos(+, -, *, /)
+# Funções: podem ser DEFindas usando o "def" (vou deixar um arquivo explicando melhor) 
+print("Olá seja bem vindo(a)!")
+UsarCalq = input("Gostaria de usar nossa a calculadora? (1 para sim, 2 para não, 3 para sair):")
+
+if UsarCalq == '1':
+    print("Entendido")
+#O código das operações devem ficar por aqui, já que o usuario escolheu usar a matemáquina
+
+elif UsarCalq == '2':
+    print("Que pena...")
+
+else:
+    print("Saindo...")
+
+#Esses são os códigos para colocar perto do "If"
+print("soma")
+#Esses são os códigos para colocar perto do "If"
+print("soma")
+def soma(a, b):
+    return a + b
+x = int(input("informe o valor de X:"))
+y = int(input("informe o valor de y:"))
+
+resultado_s = int(soma(x, y))
+print(resultado_s)
+
+#Subtração 
+print("subtração")
+def sub(a, b):
+    return a - b
+x = int(input("informe o valor de x:"))
+y = int(input("informe o valor de x:"))
+
+resultado_sub = int(sub(x, y))
+print(resultado_sub)
+
+#Multiplicação
+print("Multiplicação")
+def sub(a, b):
+    return a * b
+
+x = int(input("informe o valor de x:"))
+y = int(input("informe o valor de x:"))
+
+resultado_m = int(sub(x, y))
+print(resultado_m)
+
+#Divisão
+print("Divisão")
+def sub(a, b):
+    return a / b 
+
+x = int(input("informe o valor de x:"))
+y = int(input("informe o valor de x:"))
+
+resultado_d = int(sub(x, y))
+print(resultado_d)
+
+
+
+def soma(a, b):
+    return a + b
+x = int(input("informe o valor de X:"))
+y = int(input("informe o valor de y:"))
+
+resultado_s = int(soma(x, y))
+print(resultado_s)
+
+#Subtração 
+print("subtração")
+def sub(a, b):
+    return a - b
+x = int(input("informe o valor de x:"))
+y = int(input("informe o valor de x:"))
+
+resultado_sub = int(sub(x, y))
+print(resultado_sub)
+
+#Multiplicação
+print("Multiplicação")
+def sub(a, b):
+    return a * b
+
+x = int(input("informe o valor de x:"))
+y = int(input("informe o valor de x:"))
+
+resultado_m = int(sub(x, y))
+print(resultado_m)
+
+#Divisão
+print("Divisão")
+def sub(a, b):
+    return a / b 
+
+x = int(input("informe o valor de x:"))
+y = int(input("informe o valor de x:"))
+
+resultado_d = int(sub(x, y))
+print(resultado_d)
+
+
