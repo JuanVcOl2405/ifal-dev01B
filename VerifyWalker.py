854ddcef15f8c4ad8b8c9e0a93d0df19e7683a05
Juan Valença de Oliveira <starbea2405@gmail.com>
2024-11-11 14:35:24 -0300

    Add files via upload

---
+++ VerifyWalker.py
@@ -0,0 +1,10 @@
+print("olá ser humaninho!")
+x = int(input("Min querer saber esse valor (x):"))
+y = int(input("Min querer saber o valor desse também (y):"))
+
+if x > y:
+    print("X é maior do que Y")
+elif x < y:
+    print("Y é maior do que X")
+else:
+    print("é tudo igualzinho ser humaninho")
